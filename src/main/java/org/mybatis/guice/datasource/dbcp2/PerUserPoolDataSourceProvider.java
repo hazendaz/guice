c5673d6fd92bbc383722f0e46e5aa167235c217c
Jeremy Landis <jeremylandis@hotmail.com>
2019-11-03 15:04:36 -0500

    [enhance] Add dbcp2 in as separate to dbcp1
    review this as team dropped dbcp1 instead.

---
+++ src/main/java/org/mybatis/guice/datasource/dbcp2/PerUserPoolDataSourceProvider.java
@@ -0,0 +1,216 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.guice.datasource.dbcp2;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.sql.ConnectionPoolDataSource;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.datasources.PerUserPoolDataSource;
+
+/**
+ * Provides the Apache commons-dbcp2 {@code PerUserPoolDataSource}.
+ */
+public final class PerUserPoolDataSourceProvider implements Provider<DataSource> {
+
+  private final PerUserPoolDataSource dataSource = new PerUserPoolDataSource();
+
+  @com.google.inject.Inject(optional = true)
+  public void setConnectionPoolDataSource(ConnectionPoolDataSource cpds) {
+    dataSource.setConnectionPoolDataSource(cpds);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDataSourceName(@Named("DBCP.name") String name) {
+    dataSource.setDataSourceName(name);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDefaultAutoCommit(@Named("JDBC.autoCommit") boolean autoCommit) {
+    dataSource.setDefaultAutoCommit(autoCommit);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDefaultReadOnly(@Named("DBCP.defaultReadOnly") boolean defaultReadOnly) {
+    dataSource.setDefaultReadOnly(defaultReadOnly);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDefaultTransactionIsolation(
+      @Named("DBCP.defaultTransactionIsolation") int defaultTransactionIsolation) {
+    dataSource.setDefaultTransactionIsolation(defaultTransactionIsolation);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDescription(@Named("DBCP.description") String description) {
+    dataSource.setDescription(description);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setJndiEnvironment(@Named("DBCP.jndi.key") String key, @Named("DBCP.jndi.value") String value) {
+    dataSource.setJndiEnvironment(key, value);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setLoginTimeout(@Named("JDBC.loginTimeout") int loginTimeout) {
+    dataSource.setLoginTimeout(loginTimeout);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setMinEvictableIdleTimeMillis(@Named("DBCP.minEvictableIdleTimeMillis") int minEvictableIdleTimeMillis) {
+    dataSource.setDefaultMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setNumTestsPerEvictionRun(@Named("DBCP.numTestsPerEvictionRun") int numTestsPerEvictionRun) {
+    dataSource.setDefaultNumTestsPerEvictionRun(numTestsPerEvictionRun);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setRollbackAfterValidation(@Named("DBCP.rollbackAfterValidation") boolean rollbackAfterValidation) {
+    dataSource.setRollbackAfterValidation(rollbackAfterValidation);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setTestOnBorrow(@Named("DBCP.testOnBorrow") boolean testOnBorrow) {
+    dataSource.setDefaultTestOnBorrow(testOnBorrow);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setTestOnReturn(@Named("DBCP.testOnReturn") boolean testOnReturn) {
+    dataSource.setDefaultTestOnReturn(testOnReturn);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setTestWhileIdle(@Named("DBCP.testWhileIdle") boolean testWhileIdle) {
+    dataSource.setDefaultTestWhileIdle(testWhileIdle);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setTimeBetweenEvictionRunsMillis(
+      @Named("DBCP.timeBetweenEvictionRunsMillis") int timeBetweenEvictionRunsMillis) {
+    dataSource.setDefaultTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setValidationQuery(@Named("DBCP.validationQuery") String validationQuery) {
+    dataSource.setValidationQuery(validationQuery);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDefaultMaxTotal(@Named("DBCP.maxTotal") int maxTotal) {
+    dataSource.setDefaultMaxTotal(maxTotal);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDefaultMaxIdle(@Named("DBCP.maxIdle") int defaultMaxIdle) {
+    dataSource.setDefaultMaxIdle(defaultMaxIdle);
+  }
+
+  @com.google.inject.Inject(optional = true)
+  public void setDefaultMaxWait(@Named("DBCP.maxWait") int defaultMaxWait) {
+    dataSource.setDefaultMaxWaitMillis(defaultMaxWait);
+  }
+
+  /**
+   * Sets the per user default auto commit.
+   *
+   * @param perUserDefaultAutoCommit
+   *          the per user default auto commit
+   */
+  @com.google.inject.Inject(optional = true)
+  public void setPerUserDefaultAutoCommit(@PerUserDefaultAutoCommit Map<String, Boolean> perUserDefaultAutoCommit) {
+    for (Entry<String, Boolean> entry : perUserDefaultAutoCommit.entrySet()) {
+      dataSource.setPerUserDefaultAutoCommit(entry.getKey(), entry.getValue());
+    }
+  }
+
+  /**
+   * Sets the per user default read only.
+   *
+   * @param perUserDefaultReadOnly
+   *          the per user default read only
+   */
+  @com.google.inject.Inject(optional = true)
+  public void setPerUserDefaultReadOnly(@PerUserDefaultReadOnly Map<String, Boolean> perUserDefaultReadOnly) {
+    for (Entry<String, Boolean> entry : perUserDefaultReadOnly.entrySet()) {
+      dataSource.setPerUserDefaultReadOnly(entry.getKey(), entry.getValue());
+    }
+  }
+
+  /**
+   * Sets the per user default transaction isolation.
+   *
+   * @param perUserDefaultTransactionIsolation
+   *          the per user default transaction isolation
+   */
+  @com.google.inject.Inject(optional = true)
+  public void setPerUserDefaultTransactionIsolation(
+      @PerUserDefaultTransactionIsolation Map<String, Integer> perUserDefaultTransactionIsolation) {
+    for (Entry<String, Integer> entry : perUserDefaultTransactionIsolation.entrySet()) {
+      dataSource.setPerUserDefaultTransactionIsolation(entry.getKey(), entry.getValue());
+    }
+  }
+
+  /**
+   * Sets the per user max total.
+   *
+   * @param perUserMaxTotal
+   *          the per user max total
+   */
+  @com.google.inject.Inject(optional = true)
+  public void setPerUserMaxTotal(@PerUserMaxTotal Map<String, Integer> perUserMaxTotal) {
+    for (Entry<String, Integer> entry : perUserMaxTotal.entrySet()) {
+      dataSource.setPerUserMaxTotal(entry.getKey(), entry.getValue());
+    }
+  }
+
+  /**
+   * Sets the per user max idle.
+   *
+   * @param perUserMaxIdle
+   *          the per user max idle
+   */
+  @com.google.inject.Inject(optional = true)
+  public void setPerUserMaxIdle(@PerUserMaxIdle Map<String, Integer> perUserMaxIdle) {
+    for (Entry<String, Integer> entry : perUserMaxIdle.entrySet()) {
+      dataSource.setPerUserMaxIdle(entry.getKey(), entry.getValue());
+    }
+  }
+
+  /**
+   * Sets the per user max wait.
+   *
+   * @param perUserMaxWait
+   *          the per user max wait
+   */
+  @com.google.inject.Inject(optional = true)
+  public void setPerUserMaxWait(@PerUserMaxWait Map<String, Integer> perUserMaxWait) {
+    for (Entry<String, Integer> entry : perUserMaxWait.entrySet()) {
+      dataSource.setPerUserMaxWaitMillis(entry.getKey(), (long) entry.getValue());
+    }
+  }
+
+  @Override
+  public DataSource get() {
+    return dataSource;
+  }
+
+}
---
+++ src/test/java/org/mybatis/guice/datasource/dbcp2/PerUserPoolDataSourceModuleTest.java
@@ -0,0 +1,176 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.guice.datasource.dbcp2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import org.apache.commons.dbcp2.datasources.PerUserPoolDataSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mybatis.guice.datasource.dbcp2.PerUserPoolDataSourceModule;
+import org.mybatis.guice.datasource.dbcp2.PerUserPoolDataSourceProvider;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Provider;
+import javax.sql.ConnectionPoolDataSource;
+
+@ExtendWith(MockitoExtension.class)
+public class PerUserPoolDataSourceModuleTest {
+  @Mock
+  private ConnectionPoolDataSource connectionPoolDataSource;
+
+  @Test
+  public void configure_PerUserDefaultAutoCommit() throws Throwable {
+    Injector injector = Guice.createInjector(new PerUserPoolDataSourceModule.Builder()
+        .setPerUserDefaultAutoCommitProviderClass(PerUserDefaultAutoCommitProvider.class).create());
+    PerUserPoolDataSourceProvider provider = injector.getInstance(PerUserPoolDataSourceProvider.class);
+
+    PerUserPoolDataSource dataSource = (PerUserPoolDataSource) provider.get();
+
+    assertEquals(true, dataSource.getPerUserDefaultAutoCommit("test_user"));
+    assertEquals(false, dataSource.getPerUserDefaultAutoCommit("test_user2"));
+  }
+
+  @Test
+  public void configure_PerUserDefaultReadOnly() throws Throwable {
+    Injector injector = Guice.createInjector(new PerUserPoolDataSourceModule.Builder()
+        .setPerUserDefaultReadOnlyProviderClass(PerUserDefaultReadOnlyProvider.class).create());
+    PerUserPoolDataSourceProvider provider = injector.getInstance(PerUserPoolDataSourceProvider.class);
+
+    PerUserPoolDataSource dataSource = (PerUserPoolDataSource) provider.get();
+
+    assertEquals(false, dataSource.getPerUserDefaultReadOnly("test_user"));
+    assertEquals(true, dataSource.getPerUserDefaultReadOnly("test_user2"));
+  }
+
+  @Test
+  public void configure_PerUserDefaultTransactionIsolation() throws Throwable {
+    Injector injector = Guice.createInjector(new PerUserPoolDataSourceModule.Builder()
+        .setPerUserDefaultTransactionIsolationProviderClass(PerUserDefaultTransactionIsolationProvider.class).create());
+    PerUserPoolDataSourceProvider provider = injector.getInstance(PerUserPoolDataSourceProvider.class);
+
+    PerUserPoolDataSource dataSource = (PerUserPoolDataSource) provider.get();
+
+    assertEquals((Integer) Connection.TRANSACTION_READ_COMMITTED,
+        dataSource.getPerUserDefaultTransactionIsolation("test_user"));
+    assertEquals((Integer) Connection.TRANSACTION_SERIALIZABLE,
+        dataSource.getPerUserDefaultTransactionIsolation("test_user2"));
+  }
+
+  @Test
+  public void configure_PerUserMaxTotal() throws Throwable {
+    Injector injector = Guice.createInjector(new PerUserPoolDataSourceModule.Builder()
+        .setPerUserMaxTotalProviderClass(PerUserMaxTotalProvider.class).create());
+    PerUserPoolDataSourceProvider provider = injector.getInstance(PerUserPoolDataSourceProvider.class);
+
+    PerUserPoolDataSource dataSource = (PerUserPoolDataSource) provider.get();
+
+    assertEquals(10, dataSource.getPerUserMaxTotal("test_user"));
+    assertEquals(20, dataSource.getPerUserMaxTotal("test_user2"));
+  }
+
+  @Test
+  public void configure_PerUserMaxIdle() throws Throwable {
+    Injector injector = Guice.createInjector(new PerUserPoolDataSourceModule.Builder()
+        .setPerUserMaxIdleProviderClass(PerUserMaxIdleProvider.class).create());
+    PerUserPoolDataSourceProvider provider = injector.getInstance(PerUserPoolDataSourceProvider.class);
+
+    PerUserPoolDataSource dataSource = (PerUserPoolDataSource) provider.get();
+
+    assertEquals(30, dataSource.getPerUserMaxIdle("test_user"));
+    assertEquals(40, dataSource.getPerUserMaxIdle("test_user2"));
+  }
+
+  @Test
+  public void configure_PerUserMaxWait() throws Throwable {
+    Injector injector = Guice.createInjector(new PerUserPoolDataSourceModule.Builder()
+        .setPerUserMaxWaitProviderClass(PerUserMaxWaitProvider.class).create());
+    PerUserPoolDataSourceProvider provider = injector.getInstance(PerUserPoolDataSourceProvider.class);
+
+    PerUserPoolDataSource dataSource = (PerUserPoolDataSource) provider.get();
+
+    assertEquals(50, dataSource.getPerUserMaxWaitMillis("test_user"));
+    assertEquals(60, dataSource.getPerUserMaxWaitMillis("test_user2"));
+  }
+
+  public static class PerUserDefaultAutoCommitProvider implements Provider<Map<String, Boolean>> {
+    @Override
+    public Map<String, Boolean> get() {
+      Map<String, Boolean> defaultAutoCommit = new HashMap<String, Boolean>();
+      defaultAutoCommit.put("test_user", true);
+      defaultAutoCommit.put("test_user2", false);
+      return defaultAutoCommit;
+    }
+  }
+
+  public static class PerUserDefaultReadOnlyProvider implements Provider<Map<String, Boolean>> {
+    @Override
+    public Map<String, Boolean> get() {
+      Map<String, Boolean> defaultAutoCommit = new HashMap<String, Boolean>();
+      defaultAutoCommit.put("test_user", false);
+      defaultAutoCommit.put("test_user2", true);
+      return defaultAutoCommit;
+    }
+  }
+
+  public static class PerUserDefaultTransactionIsolationProvider implements Provider<Map<String, Integer>> {
+    @Override
+    public Map<String, Integer> get() {
+      Map<String, Integer> defaultTransactionIsolation = new HashMap<String, Integer>();
+      defaultTransactionIsolation.put("test_user", Connection.TRANSACTION_READ_COMMITTED);
+      defaultTransactionIsolation.put("test_user2", Connection.TRANSACTION_SERIALIZABLE);
+      return defaultTransactionIsolation;
+    }
+  }
+
+  public static class PerUserMaxTotalProvider implements Provider<Map<String, Integer>> {
+    @Override
+    public Map<String, Integer> get() {
+      Map<String, Integer> maxTotal = new HashMap<String, Integer>();
+      maxTotal.put("test_user", 10);
+      maxTotal.put("test_user2", 20);
+      return maxTotal;
+    }
+  }
+
+  public static class PerUserMaxIdleProvider implements Provider<Map<String, Integer>> {
+    @Override
+    public Map<String, Integer> get() {
+      Map<String, Integer> maxIdle = new HashMap<String, Integer>();
+      maxIdle.put("test_user", 30);
+      maxIdle.put("test_user2", 40);
+      return maxIdle;
+    }
+  }
+
+  public static class PerUserMaxWaitProvider implements Provider<Map<String, Integer>> {
+    @Override
+    public Map<String, Integer> get() {
+      Map<String, Integer> maxWait = new HashMap<String, Integer>();
+      maxWait.put("test_user", 50);
+      maxWait.put("test_user2", 60);
+      return maxWait;
+    }
+  }
+}
